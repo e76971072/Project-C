999696344ebfa80fe3b8295e26084c5e4bef859a
Kevin <32458736+e76971072@users.noreply.github.com>
2018-09-16 17:13:08 -0500

    first commit

---
+++ HW1.h
@@ -0,0 +1,20 @@
+//
+//  HW1.h
+//  
+//
+//  Created by Mr Kevin on 9/12/18.
+//
+
+#ifndef HW1_h
+#define HW1_h
+
+
+#endif /* HW1_h */
+
+
+typedef struct puzzle{
+    
+    int number;
+    
+    char possible;
+} puzzle;
